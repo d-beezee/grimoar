8157a734d104fd226c01436049d17a80df700e63
Davide Bizzi <davide.bizzi@unguess.io>
2024-11-25 16:50:28 +0100

    server token

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,13 @@ const config: CapacitorConfig = {
   appId: "com.clochard.grimoar",
   appName: "Grimoar",
   webDir: "dist",
+  plugins: {
+    GoogleAuth: {
+      scopes: ["profile", "email"],
+      serverClientId:
+        "976308014328-c5sjeutgm9bcjk289hm1khihjkmpc6c5.apps.googleusercontent.com",
+    },
+  },
 };
 
 export default config;
